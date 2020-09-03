c3bc09ed66a0fa3bb4233cb3f1dc4ab7807a60b8
Paulina Studniak <paulinaw2612@gmail.com>
2020-09-03 14:16:12 +0200

    html structure, styles and functionality according local storage added

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,29 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const textarea = document.querySelector(".textarea--js");
+
+const save = document.querySelector(".save--js");
+
+const load = document.querySelector(".load--js");
+
+const currentValue = localStorage.getItem("entry");
+
+if (currentValue) {
+  document.querySelector(".info--js").innerHTML = "❗️";
+}
+
+save.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.setItem("entry", textarea.value);
+  if (textarea.value) {
+    document.querySelector(".info--js").innerHTML = "❕";
+  } else {
+    document.querySelector(".info--js").innerHTML = "";
+  }
+});
+
+load.addEventListener("click", (e) => {
+  e.preventDefault();
+  textarea.value = localStorage.getItem("entry");
+});
